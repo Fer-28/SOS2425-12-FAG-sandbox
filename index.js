90dbfaa8506d2e539a86232aea13393c1a7eb6c8
Fer-28 <feragugar@alum.us.es>
2025-03-21 11:47:22 +0100

    Nomentero

--- index.js
+++ index.js
@@ -1,32 +1,15 @@
-const express = require("express");
+//const express = require("express");
+import express from "express";
+import  { loadBackend } from "./src/back/index.js";
+
 const app = express();
 const PORT = process.env.PORT || 16078;
-const BASE_API = "/api/v1";
 
-app.use("/", express.static("./public"));
+app.use(express.json());
+app.use("/",express.static("./public"));
 
-let contacts = [
-    {
-        name: "peter",
-        phone: 123456
-    },
-    {
-        name: "pablo",
-        phone: 987654
-    }
-];
+loadBackend(app)
 
-app.get(BASE_API+"/contacts",(request, response)=>{
-    console.log("New GET to /contacts");
-    response.send(JSON.stringify(contacts));
-});
-
-app.get("/hello", (request, response) => {
-    response.send("Hello from the server!");
-});
-
-app.listen(PORT, () => {
+app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}!`);
 });
-
-console.log(`Finish setup`);
